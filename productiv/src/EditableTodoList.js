d483519324f32fc48c7315a08763711840394293
Graham MacFarquhar <gmacfarquhar57@gmail.com>
2023-05-25 15:05:53 -0700

    first commit

---
+++ productiv/src/EditableTodoList.js
@@ -0,0 +1,30 @@
+import React from "react";
+import EditableTodo from "./EditableTodo";
+
+/** Show list of editable todos.
+ *
+ * Props:
+ * - todos: array of [ todo, ... ]
+ * - update(): fn to call to update a todo
+ * - remove(): fn to call to remove a todo
+ *
+ * TodoApp -> EditableTodoList -> [ EditableTodo, ... ]
+ */
+
+function EditableTodoList({ todos, update, remove }) {
+  console.log("todos list",todos)
+  return (
+    <div>
+      {todos.map(t =>
+        <EditableTodo
+          key={t.id}
+          todo={t}
+          update={update}
+          remove={remove}
+        />
+      )}
+    </div>
+  );
+}
+
+export default EditableTodoList;
---
+++ productiv/src/Todo.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+/** Simple presentation component for a todo.
+ *
+ * Props:
+ * - todo: like { id, title, description, priority }
+ *
+ * { EditableTodo, TopTodo } -> Todo
+ **/
+
+function Todo({todo}) {
+  //const [todo, setTodo] = useState(todo)
+  console.log(todo);
+  console.log(todo.title);
+
+  return (
+     <div className="Todo">
+        <div><b>{todo.title}</b> <small>{todo.priority}</small></div>
+        <div><small>{todo.description}</small></div>
+      </div>
+  );
+}
+
+export default Todo;
